58ab0e6291be5d9611bf8e0f0f46f950e43d5877
Ricarvy <597928240@qq.com>
2017-12-28 19:18:42 +0800

    upload

---
+++ Test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/28 17:14
+# @Author  : Li Jiawei
+# @FileName: Test.py
+# @Software: PyCharm
+
+import fool
+
+text='本次冬令营选拔了32人组成国家集训队'
+print(fool.cut(text=text))
